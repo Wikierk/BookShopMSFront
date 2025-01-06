e16d4ee3af36613841b6f641ef2a8a84fd1cbdd7
Wojciech Opara <wwojtek660@gmail.com>
2025-01-06 15:34:39 +0100

    Connect to the server via a socket

---
+++ src/bookshopmanagementsystem/Client.java
@@ -0,0 +1,27 @@
+package bookshopmanagementsystem;
+
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) throws IOException {
+        socket = new Socket(ip, port);
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public String sendMessage(String message) throws IOException {
+        out.println(message);
+        return in.readLine();
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+}
